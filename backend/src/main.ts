4e9fa8d91ecd7e112347753952116892707d966b
rexparmar <parmarrex114@gmail.com>
2026-03-29 13:29:59 -0400

    Basic health check server.

---
+++ backend/src/main.ts
@@ -0,0 +1,25 @@
+import 'reflect-metadata';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const port = process.env.PORT || 5000;
+  await app.listen(port);
+
+  console.log(`Server running on http://localhost:${port}`);
+}
+
+bootstrap();
